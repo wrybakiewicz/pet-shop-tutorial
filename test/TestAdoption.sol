9acf57c1512e18a55742f570738816fc3bb396eb
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-01-27 21:41:10 +0100

    add adoption contract & test

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -1,7 +1,31 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
 
 contract TestAdoption {
-    function TestAdoption(){
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
 
+    uint expectedPetId = 0;
+
+    address expectedAdopter = address(this);
+
+    function testUserCanAdoptPet() public {
+        uint returnedId = adoption.adopt(expectedPetId);
+
+        Assert.equal(returnedId, expectedPetId, "Adoption of the expected pet should match what is returned");
+    }
+
+    function testGetAdopterAddressByPetId() public {
+        address resultAdopter = adoption.adopters(expectedPetId);
+
+        Assert.equal(resultAdopter, expectedAdopter, "Owner of the expected pet should be this contract");
+    }
+
+    function testGetAdopterAddressByPetIdInArray() public {
+        address[16] memory allAdopters = adoption.getAdopters();
+
+        Assert.equal(allAdopters[expectedPetId], expectedAdopter, "Owner of the expected pet should be this contract");
     }
 }
